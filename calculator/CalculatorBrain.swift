f5ec76843aead4d196d0072b02e9e9b592a97a86
Linspery <464826172@qq.com>
2015-04-08 16:25:09 +0800

    class4

--- calculator/CalculatorBrain.swift
+++ calculator/CalculatorBrain.swift
@@ -49,7 +49,27 @@ class CalculatorBrain {
 //        knownOps["−"] = Op.BinaryOperand("−"){$1-$0}
 //        knownOps["√"] = Op.UnaryOperand("√", sqrt)
     }
-    
+    var program:AnyObject{//guaranteed to be a PropretyList
+        get{
+            return opStack.map{$0.description}
+        }
+        set{
+            if let opSymbols = newValue as? Array<String>{
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols{
+                    if let op = knownOps[opSymbol]{
+                        newOpStack.append(op)
+                    }else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue{
+                        
+                        newOpStack.append(.Operand(operand))
+                    }
+                    opStack = newOpStack
+                }
+            }
+            
+        }
+    }
+
     private func evaluate(ops:[Op]) -> (result : Double? , remainingOps:[Op]){
         if !ops.isEmpty{
             var remainingOps = ops
